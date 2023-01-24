fae6b138b1599042857389d0be2e79385910144b
Luke Dixon <github@lukedixon.nl>
2023-01-24 15:50:31 +0100

    got audio element working with manipulation

--- js/soundBaseComponent.js
+++ js/soundBaseComponent.js
@@ -2,8 +2,36 @@ AFRAME.registerComponent('soundcomponent', {
   schema: {
     name: { type: 'string' },
     src: { type: 'string' },
-    radius: { type: 'string', default: '0.05' },
+    radius: { type: 'string', default: '0.3s' },
   },
+  init: function () {
+    console.log(this.data.src);
+    let clicked = false;
+    let el = this.el;
+    let pos = el.getAttribute('position');
+    let audio = new Audio(this.data.src);
+    el.setAttribute('rotation', { x: 0, y: -00, z: -90 });
+    el.setAttribute('geometry', 'radius', this.data.radius);
+    el.setAttribute('material', 'color', 'rgb(49, 65, 78)');
+    el.setAttribute('geometry', 'height', '0.1');
+    el.object3D.position.set(pos.x, pos.y, pos.z);
+    el.classList.add('js--interact');
+
+    this.PlayFragment = () => {
+      clicked = true;
+      if (!clicked) {
+        el.setAttribute('material', 'color', 'rgb(49, 65, 78)');
+      } else {
+        el.setAttribute('material', 'color', 'rgb(213, 106, 39)');
+        audio.play();
+        audio.addEventListener('ended', () => {
+          el.setAttribute('material', 'color', 'rgb(49, 65, 78)');
+        });
+      }
+    };
+    el.addEventListener('click', this.PlayFragment);
+  },
+  update: function () {},
 });
 
 AFRAME.registerPrimitive('a-soundFragment', {
@@ -13,8 +41,8 @@ AFRAME.registerPrimitive('a-soundFragment', {
   },
 
   mappings: {
-    name: 'soundcomponent',
-    src: '',
-    radius: '',
+    name: 'soundcomponent.name',
+    src: 'soundcomponent.src',
+    radius: 'soundcomponent.radius',
   },
 });
